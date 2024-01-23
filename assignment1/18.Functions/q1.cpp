c86bbf3f753edaa53202621e9d1bf83bdab70505
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-01-23 12:00:18 +0600

    18. functions

---
+++ assignment1/18.Functions/q1.cpp
@@ -0,0 +1,21 @@
+/*
+1. Write a function named `max` that takes two numbers as arguments and returns the larger of them.
+*/
+
+#include <iostream>
+using namespace std;
+
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Max of " << a << " and " << b << " is " << max(a, b) << endl;
+    return 0;
+}
---
+++ assignment1/18.Functions/q2.cpp
@@ -0,0 +1,18 @@
+/*
+2. Create a function that calculates and returns the area of a circle given its radius.
+*/
+
+#include <iostream>
+using namespace std;
+
+double areaOfCircle(double radius) {
+    return 3.14 * radius * radius;
+}
+
+int main() {
+    double radius;
+    cout << "Enter radius: ";
+    cin >> radius;
+    cout << "Area of circle with radius " << radius << " is " << areaOfCircle(radius) << endl;
+    return 0;
+}

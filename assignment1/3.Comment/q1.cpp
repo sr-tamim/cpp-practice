bd515b7659457845a93c63c77a0fce7fa3584d9d
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-01-23 10:14:13 +0600

    3. comment solve

---
+++ assignment1/3.Comment/q1.cpp
@@ -0,0 +1,14 @@
+/*
+1. Write a program that adds two numbers and use single-line comments to explain each step.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 12;
+    int b = 4;
+    int sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is " << sum << endl;
+    return 0;
+}
---
+++ assignment1/3.Comment/q2.cpp
@@ -0,0 +1,14 @@
+/*
+2. Write a program that calculates the area of a rectangle and use multi-line comments to describe the formula.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length = 12;
+    int width = 4;
+    int area = length * width;
+    cout << "The area of a rectangle with length " << length << " and width " << width << " is " << area << endl;
+    return 0;
+}

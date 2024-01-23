6c672573f641cfa01c3685a3e62786b74406c49c
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-01-23 10:22:57 +0600

    10. for loop solve

---
+++ assignment1/10.For_Loop/q1.cpp
@@ -0,0 +1,15 @@
+/*
+1. Write a for loop that prints the first 10 multiples of 3.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        cout << 3 * i << endl;
+    }
+
+    return 0;
+}
---
+++ assignment1/10.For_Loop/q2.cpp
@@ -0,0 +1,15 @@
+/*
+2. Create a for loop that prints the square of numbers from 1 to 5.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        cout << i * i << endl;
+    }
+
+    return 0;
+}

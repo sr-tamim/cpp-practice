ca1c85db2b7014711f3059714c417fccf90fc6d1
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-01-23 10:13:08 +0600

    hello world solve

---
+++ assignment1/1.Hello_World/q1.cpp
@@ -0,0 +1,10 @@
+/*
+1. Write a program that prints "Learning C++ is fun!".
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Learning C++ is fun!" << endl;
+    return 0;
+}
---
+++ assignment1/1.Hello_World/q2.cpp
@@ -0,0 +1,10 @@
+/*
+2. Write a program that prints your name followed by "starts coding in C++!".
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "SR Tamim" << " starts coding in C++!" << endl;
+    return 0;
+}

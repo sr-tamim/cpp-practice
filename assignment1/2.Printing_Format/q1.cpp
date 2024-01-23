eab8d50d92158faf3e1b2afd57556c65f3b945d1
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-01-23 10:14:01 +0600

    2. printing format solve

---
+++ assignment1/2.Printing_Format/q1.cpp
@@ -0,0 +1,12 @@
+/*
+1. Print the multiplication and division of two numbers, 12 and 4.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "12 * 4 = " << 12 * 4 << endl;
+    cout << "12 / 4 = " << 12 / 4 << endl;
+    return 0;
+}
---
+++ assignment1/2.Printing_Format/q2.cpp
@@ -0,0 +1,13 @@
+/*
+2. Print the full name by combining two string variables, firstName and lastName.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string firstName = "SR";
+    string lastName = "Tamim";
+    cout << firstName << " " << lastName << endl;
+    return 0;
+}

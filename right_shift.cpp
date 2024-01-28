5dc2901483dcba9d2bcc2571b17171bef6052e9b
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-01-29 00:09:27 +0600

    dynamic array right shift

--- right_shift.cpp
+++ right_shift.cpp
@@ -1,30 +1,47 @@
 #include<iostream>
 using namespace std;
 
+// right shift an array by n times
+void arrShiftRight(int arr[], int arrSize, int k)
+{
+    for (int i = arrSize - 1; i >= 0; i--)
+        arr[i] = (i < k) ? 0 : arr[i - k];
+}
+
+// print array
+void printArray(int arr[], int arrSize)
+{
+    for (int i = 0; i < arrSize; i++)
+        cout << arr[i] << (i == arrSize - 1 ? "\n" : ", ");
+}
+
 int main()
 {
-    int arr[5] = {3, 8, 12, 9, 44};
-    int arrLength = sizeof(arr) / sizeof(arr[0]);
+    int arrSize;
+    cout << "Enter array size: ";
+    cin >> arrSize;
+
+    int arr[arrSize];
+    cout << "Enter array elements: " << endl;
+    for (int i = 0; i < arrSize; i++)
+        cin >> arr[i];
+
     cout << "Current arr = ";
-    for (int i = 0; i < arrLength; i++)
-        cout << arr[i] << ", ";
+    printArray(arr, arrSize);
 
     int maxShift = 1;
     cout << endl << "How many times you want to Right shift the array? ";
     cin >> maxShift;
 
-    if (maxShift > arrLength)
+    if (maxShift > arrSize)
     {
         cout << "Shifting value exceeded the array length!";
         return 1;
     }
 
-    for (int i = arrLength - 1; i >= 0; i--)
-        arr[i] = (i < maxShift) ? 0 : arr[i - maxShift];
+    arrShiftRight(arr, arrSize, maxShift);
 
     cout << endl << "Right shifted arr = ";
-    for (int i = 0; i < arrLength; i++)
-        cout << arr[i] << ", ";
-    cout << endl;
+    printArray(arr, arrSize);
     return 0;
 }
